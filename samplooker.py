0eadc87f45215f9c183927939d9c0803bc99c7e7
Naor Movshovitz <nmovshov@gmail.com>
2022-08-08 12:05:27 -0700

    NMoI hister.

--- samplooker.py
+++ samplooker.py
@@ -12,3 +12,20 @@ def load_planets(fname):
         planets = pickle.load(f)
         print(f"Found {len(planets)} planets in {fname}.")
     return planets
+
+def hist_moi(fname, bins='auto', density=True, **kwargs):
+    # Prepare the data
+    planets = load_planets(fname)
+    ice = np.array([p.NMoI for p in planets])
+
+    # Prepare the canvas
+    plt.figure(figsize=(8,6))
+
+    # Plot the histogram
+    plt.hist(ice, bins=bins, density=density, **kwargs)
+
+    # Style, annotate, and show
+    plt.xlabel(r'Normalized moment of inertia, $I/Ma_0^2$')
+    plt.xlim(min(ice),max(ice))
+    plt.yticks([])
+    plt.show(block=False)
