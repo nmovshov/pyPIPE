93e41eb283611668bf481422cea801d91698d2a3
Naor Movshovitz <nmovshov@gmail.com>
2022-12-10 14:18:05 -0800

    Unifying mPIPE and pyPIPE in one root directory.

--- setws.m
+++ setws.m
@@ -2,6 +2,4 @@ function setws()
 % Set workspace (paths and/or common variables).
 
 addpath(pwd)
-addpath(fullfile(pwd, 'mPIPE'))
-addpath(fullfile(pwd, 'mPIPE', 'extern'))
-addpath(fullfile(pwd, 'mPIPE', 'TOF-planet'))
+addpath(fullfile(pwd, 'extern'))
