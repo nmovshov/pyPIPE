117941d02c99aacf944227d643901c8081b1e00f
Naor Movshovitz <nmovshov@gmail.com>
2023-01-27 11:04:14 -0800

    bsaeline up to date uranus deg6

--- setws.py
+++ setws.py
@@ -4,6 +4,8 @@ import sys, os
 import numpy as np
 if sys.platform == 'win32':
     import matplotlib.pyplot as plt
+    from IPython import get_ipython
+    get_ipython().run_line_magic('matplotlib','')
 
 sys.path.append(os.getcwd())
 
