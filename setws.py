66fe445f69701b0a7f23c561e3b656d85406412c
Naor Movshovitz <nmovshov@gmail.com>
2023-02-06 17:30:36 -0800

    tweak setws.py

--- setws.py
+++ setws.py
@@ -4,6 +4,7 @@ import sys, os
 import numpy as np
 if sys.platform == 'win32':
     import matplotlib.pyplot as plt
+    plt.style.use('ndefault')
     from IPython import get_ipython
     get_ipython().run_line_magic('matplotlib','')
 
