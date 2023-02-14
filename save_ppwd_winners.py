23dcc4304b5fa4a28b213ccccd5d0cbd8b71e96d
Naor Movshovitz <nmovshov@gmail.com>
2023-02-13 18:28:17 -0800

    working on stricter interpretation of winners for quenching runs

--- save_ppwd_winners.py
+++ save_ppwd_winners.py
@@ -1,43 +1,106 @@
-import sys
-import os
+#------------------------------------------------------------------------------
+# Script to select winners from a ppwd-sample emcee chain.
+#------------------------------------------------------------------------------
+import sys, os
 import numpy as np
+import argparse
 import ppwd
 import ahelpers as ah
 
-if len(sys.argv) < 5:
-    print("Usage:")
-    print("  save_ppwd_winners.py filename outputname dof fixrot {seedfile}")
-    sys.exit(0)
+import observables
+import TOFPlanet
+import ahelpers as ah
+import ppwd
+the_mdl = ppwd.ppwd_profile
+the_transform = ppwd.ppwd_transform
 
-fname = sys.argv[1]
-outname = sys.argv[2]
-df = int(sys.argv[3])
-rotflag = int(sys.argv[4])
-seedfile = sys.argv[5] if len(sys.argv) > 5 else None
+def _main(args):
+    # Load the raw emcee chain
+    C,L = ah.load_chain(args.chainfile)
+    print()
+    print("C.shape = ", C.shape)
 
-C,L = ah.load_chain(fname)
-print()
-print("C.shape = ", C.shape)
-keepers = ah.winners(C,L,df,ppwd.ppwd_prior,rotflag) # 2.5 sigma with n dof
-print("keepers.shape = ", keepers.shape)
-Z = C[keepers,-1,:]
-if Z.ndim == 1:
-    Z = Z.reshape((1,-1))
-print("Z.shape = ", Z.shape)
+    # Candidates have log-like value (minus prior) euiv to 2.5 sigma with n dof
+    keepers = ah.winners(C,L,args.dof,ppwd.ppwd_prior,args.fix_rot)
+    print("keepers.shape = ", keepers.shape)
+    Z = C[keepers,-1,:]
+    if Z.ndim == 1:
+        Z = Z.reshape((1,-1))
+    print("Z.shape = ", Z.shape)
 
-if seedfile and Z.size:
-    S = np.loadtxt(seedfile)
-    fakers = []
-    for z in Z:
-        if z in S:
-            fakers.append(True)
-        else:
-            fakers.append(False)
-    Z = Z[~np.array(fakers)]
-    print(f"found and removed {sum(fakers)} fakers.")
+    # Some of them could be fakers from original seed
+    if args.seedfile and Z.size:
+        S = np.loadtxt(args.seedfile)
+        fakers = []
+        for z in Z:
+            if z in S:
+                fakers.append(True)
+            else:
+                fakers.append(False)
+        Z = Z[~np.array(fakers)]
+        print(f"found and removed {sum(fakers)} fakers.")
 
-print(f"appending {Z.shape} to {outname}.")
-try:
-    ah._append(outname, Z)
-except:
-    np.savetxt(outname, Z)
+    # abyu
+    print(f"appending {Z.shape} to {args.outname}.")
+    try:
+        ah._append(args.outname, Z)
+    except:
+        np.savetxt(args.outname, Z)
+
+def _PCL():
+    # Return struct with command line arguments as fields.
+    parser = argparse.ArgumentParser(
+        description="Select winners from tip of emcee chain.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('chainfile', help="npz file with emcee chain")
+    parser.add_argument('outname', help="where to append winners to")
+    parser.add_argument('dof', type=int,
+        help="how many degrees of freedom in log-like")
+
+    parser.add_argument('-s','--seedfile',
+        help="Seed file used by driver")
+
+    parser.add_argument('--J-strict', type=int, default=0,
+        help="passing positive J-strict will require winners to " +
+        "satisfy individual J values in addition to chi2 threshold")
+
+    parser.add_argument('-o','--observables',
+            help="Observables struct (usually planet name, " +
+                 "see observables.py for options")
+
+    mdlgroup = parser.add_argument_group('Additional model options')
+
+    mdlgroup.add_argument('--fix-mass', type=int, default=1,
+        help="Normalize converged model mass to obs.M")
+
+    mdlgroup.add_argument('--fix-rot', type=int, default=1,
+        help="Don't sample rotation period (use obs.P instead)")
+
+    mdlgroup.add_argument('--preserve-period', type=int, default=1,
+        help="Iterate on rotation m until rotation period matched obs.P.")
+
+    mdlgroup.add_argument('--no-spin', action='store_true',
+        help="Make spherical planet (sets obs.P to inf)")
+
+    mdlgroup.add_argument('--with-k2', action='store_true',
+        help="Include tidal k2 calculation")
+
+    tofgroup = parser.add_argument_group('TOF options',
+        'Options controlling ToF gravity calculation')
+
+    tofgroup.add_argument('--toforder', type=int, default=4, choices=[4,7],
+        help="Theory of figures expansion order")
+
+    tofgroup.add_argument('--toflevels', type=int, default=4096,
+        help="Number of level surfaces used to discretize density profile")
+
+    tofgroup.add_argument('--xlevels', type=int, default=256,
+        help="Skip-n-spline levels")
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    clargs = _PCL()
+    _main(clargs)
